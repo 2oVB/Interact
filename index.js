d66cff836cba7c3f120778918291466ee3cab121
ville2008 <40721554+ville2008@users.noreply.github.com>
2018-09-25 12:19:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+let cors = require('cors');
+
+app.use(cors);
+
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
